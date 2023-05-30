ac0fbd484d3d5f50177b78ca3f3df7f760ff6d9c
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-05-30 12:53:05 -0300

    Create entities Recipe and Expense

---
+++ src/main/java/com/example/entities/Expense.java
@@ -0,0 +1,27 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_expenses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal price;
+    @Column(name = "date_time")
+    private LocalDate date = LocalDate.now();
+}
