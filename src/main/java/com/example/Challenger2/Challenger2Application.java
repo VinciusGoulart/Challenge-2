1e1e5aed3f32ab58860568aeff96833fa62be434
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-05-30 11:44:24 -0300

    Project started

---
+++ src/main/java/com/example/Challenger2/Challenger2Application.java
@@ -0,0 +1,13 @@
+package com.example.Challenger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenger2Application.class, args);
+	}
+
+}
