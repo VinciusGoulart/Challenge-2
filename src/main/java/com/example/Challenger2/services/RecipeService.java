fadb1c787fe29f4c9476f7df593130bb78e8e9ae
Vladimir Coutinho <VladimirCoutinho@unitins.br>
2023-05-31 21:56:46 -0300

    Created getters endpoint, findAll and findById

--- src/main/java/com/example/Challenger2/services/RecipeService.java
+++ src/main/java/com/example/Challenger2/services/RecipeService.java
@@ -4,6 +4,7 @@ import com.example.Challenger2.entities.Recipe;
 import com.example.Challenger2.entities.recipeDTOs.RecipeDTO;
 import com.example.Challenger2.repositories.RecipeRepository;
 import com.example.Challenger2.services.exceptions.BadRequestException;
+import com.example.Challenger2.services.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,20 +14,29 @@ import java.util.List;
 public class RecipeService {
 
     @Autowired
-    RecipeRepository repository;
+    private RecipeRepository recipeRepository;
 
     public Recipe save(RecipeDTO recipe) {
         if (checkDescription(recipe)) {
             throw new BadRequestException("This recipe already exists");
         }
-        return repository.save(new Recipe(recipe));
+        return recipeRepository.save(new Recipe(recipe));
+    }
+
+    public List<RecipeDTO> findAll() {
+        return recipeRepository.findAll().stream().map(RecipeDTO::new).toList();
+    }
+
+    public Recipe findById(Long id) {
+        return recipeRepository.findById(id).orElseThrow(() -> new NotFoundException("ID Not Found"));
     }
 
     private Boolean checkDescription(RecipeDTO recipeDTO) {
         Integer monthValue = recipeDTO.getDate().getMonthValue();
         Integer yearValue = recipeDTO.getDate().getYear();
-        List<Recipe> list = repository.findByDescriptionAndDateMonthAndDateYearAllIgnoreCase(recipeDTO.getDescription(), monthValue, yearValue);
+        List<Recipe> list = recipeRepository.findByDescriptionAndDateMonthAndDateYearAllIgnoreCase(recipeDTO.getDescription(), monthValue, yearValue);
         return !list.isEmpty();
     }
+
 
 }
