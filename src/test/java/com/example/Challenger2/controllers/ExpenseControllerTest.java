13b8c895791d16899b8e9a06abd191009ec6a396
ViniciusGoulart <viniciusbgoulart@hotmail.com>
2023-06-14 10:46:06 -0300

    Created the appropriate tests for expense update,delete and find by month

--- src/test/java/com/example/Challenger2/controllers/ExpenseControllerTest.java
+++ src/test/java/com/example/Challenger2/controllers/ExpenseControllerTest.java
@@ -3,7 +3,6 @@ package com.example.Challenger2.controllers;
 import com.example.Challenger2.entities.DTOs.expenseDTOs.ExpenseDTO;
 import com.example.Challenger2.entities.Expense;
 import com.example.Challenger2.entities.enums.Category;
-import com.example.Challenger2.services.ExpenseService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,8 +22,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
@@ -32,9 +30,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 class ExpenseControllerTest {
     @Autowired
     private MockMvc mvc;
-    @Autowired
-    private ExpenseService expenseService;
-
     private JacksonTester<ExpenseDTO> dtoJacksonTester;
 
     @BeforeEach
@@ -166,7 +161,7 @@ class ExpenseControllerTest {
 
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-        assertThat(response.getContentAsString()).contains("test");
+        assertThat(response.getContentAsString()).doesNotContain("banana");
     }
 
     @Test
@@ -182,5 +177,137 @@ class ExpenseControllerTest {
 
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(response.getContentAsString()).doesNotContain("banana");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Must return code 200 and update the expense")
+    void update1() throws Exception {
+        Long id = 1L;
+
+        ExpenseDTO dto = new ExpenseDTO(new Expense(null, "test2", new BigDecimal(2500),
+                LocalDate.of(1999, 10, 10), Category.Food));
+
+        var response = mvc.perform(put("/expenses/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJacksonTester.write(dto).getJson()))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Must return code 404 if expense does not exists")
+    void update2() throws Exception {
+        Long id = 999999999L;
+
+        ExpenseDTO dto = new ExpenseDTO(new Expense(null, "test2", new BigDecimal(2500),
+                LocalDate.of(1999, 10, 10), Category.Food));
+
+        var response = mvc.perform(put("/expenses/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJacksonTester.write(dto).getJson()))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Must return code 400 if business rules are not valid")
+    void update3() throws Exception {
+        Long id = 1L;
+
+        ExpenseDTO dto = new ExpenseDTO(new Expense(null, "", new BigDecimal(2500),
+                LocalDate.of(1999, 10, 10), Category.Food));
+        ExpenseDTO dto2 = new ExpenseDTO(new Expense(null, "test4", new BigDecimal(-2500),
+                LocalDate.of(1999, 10, 10), Category.Food));
+        ExpenseDTO dto3 = new ExpenseDTO(new Expense(null, "test4", new BigDecimal(2500),
+                LocalDate.of(2100, 10, 10), Category.Food));
+        ExpenseDTO dto4 = new ExpenseDTO(new Expense(null, "test4", new BigDecimal(2500),
+                LocalDate.of(1999, 10, 10), null));
+
+        var response = mvc.perform(put("/expenses/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJacksonTester.write(dto).getJson()))
+                .andReturn().getResponse();
+        var response2 = mvc.perform(put("/expenses/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJacksonTester.write(dto2).getJson()))
+                .andReturn().getResponse();
+        var response3 = mvc.perform(put("/expenses/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJacksonTester.write(dto3).getJson()))
+                .andReturn().getResponse();
+        var response4 = mvc.perform(put("/expenses/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dtoJacksonTester.write(dto4).getJson()))
+                .andReturn().getResponse();
+
+        // Verify that the system correctly validates the blank description and returns the proper response code.
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        // Verify that the system correctly validates the negative value and returns the proper response code.
+        assertThat(response2.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        // Verify that the system correctly validates the future date and returns the proper response code.
+        assertThat(response3.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
+        // Verifies that the system correctly validates the invalid category and returns the appropriate response code.
+        assertThat(response4.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Must return 204 if expense is deleted")
+    void delete1() throws Exception {
+        Long id = 1L;
+
+        var response = mvc.perform(delete("/expenses/{id}", id)).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Must return 404 if expense does not exists")
+    void delete2() throws Exception {
+        Long id = 9999999L;
+
+        var response = mvc.perform(delete("/expenses/{id}", id)).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("You must return code 200 and all expenses with the date provided")
+    void findByYearAndMonth() throws Exception {
+        Integer year = 1999;
+        Integer month = 2;
+
+        var response = mvc.perform(get("/expenses/{year}/{month}", year, month)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString()).doesNotContain("1800-02");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("Must return code 404 if the expense does not exist on the indicated date")
+    void findByYearAndMonth2() throws Exception {
+        Integer year = 2023;
+        Integer month = 5;
+
+        var response = mvc.perform(get("/expenses/{year}/{month}", year, month)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        assertThat(response.getContentAsString()).doesNotContain("2023-05");
     }
 }
